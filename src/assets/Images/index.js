b8b7d16288f3044735671a90b8df6ae11938ff08
pritalakzmi <2118126@scholar.itn.ac.id>
2023-11-13 11:45:44 +0700

    [GojekApp-UI]

---
+++ src/assets/Images/index.js
@@ -0,0 +1,25 @@
+import iklan from './iklan.jpg';
+import box from './box.png';
+import bill from './bill.png';
+import more from './more.png';
+import scooter from './scooter.png';
+import shopping from './shopping-basket.png';
+import spoon from './spoon.png';
+import train from './train.png';
+import car from './hatchback.png';
+import driver from './driver.jpg';
+import download from './download.png';
+
+export {
+  iklan,
+  box,
+  bill,
+  more,
+  scooter,
+  shopping,
+  spoon,
+  train,
+  car,
+  driver,
+  download,
+};
